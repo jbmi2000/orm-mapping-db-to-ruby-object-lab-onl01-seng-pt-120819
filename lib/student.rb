2a8caa6bc7b0412d16066d2609a075179bc8ab58
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-01 16:53:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,9 +84,9 @@ class Student
     LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map.first do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.first
 
   end
 
