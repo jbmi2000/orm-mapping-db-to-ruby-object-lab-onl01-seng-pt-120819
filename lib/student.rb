685d8c8e99bfb1ee6b4f8b0c8ace253bb5834589
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-01 16:50:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,7 +84,10 @@ class Student
     LIMIT 1
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map.first do |row|
+      self.new_from_db(row)
+    end
+    
   end
 
   def self.all_students_in_grade_X(x)
